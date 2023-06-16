c2a7fe5fa5dca28246f65950cc578a91586bbfb2
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2023-06-16 10:02:45 -0300

    Add files via upload

--- src/main/java/api/momento/data/momento/repository/PaisRepository.java
+++ src/main/java/api/momento/data/momento/repository/PaisRepository.java
@@ -3,8 +3,10 @@ package api.momento.data.momento.repository;
 import api.momento.data.momento.models.Cargo;
 import api.momento.data.momento.models.Pais;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 @Repository
@@ -14,5 +16,8 @@ public interface PaisRepository extends JpaRepository<Pais, UUID> {
 
 
     Optional<Pais> findPaisBypaisNome(String nome);
+
+    @Query(value = "SELECT p.* FROM pais p INNER JOIN regiao r ON p.regiao_id = r.regiao_id WHERE  p.regiao_id = ?1", nativeQuery = true)
+    List<Pais> findPaisByregiaoId(int id);
 }
 
